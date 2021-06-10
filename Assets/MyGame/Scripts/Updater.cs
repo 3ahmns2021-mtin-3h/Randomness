2c8f07944e3b25646d21a628dc3a7da9d50f5721
smeerws <hello@smeerws.at>
2021-06-10 07:14:24 +0200

    init Ref Prj Randomness

---
+++ Assets/MyGame/Scripts/Updater.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Updater : MonoBehaviour {
+
+    [SerializeField] private Text randomText;
+    [SerializeField] private GameObject circleColor;
+    [SerializeField] private GameObject circleObjPosition, circPosLeft, circPosRight;
+    [SerializeField] private GameObject[] circleSizeObjects;
+    [SerializeField] private List<Image> colorImages;
+
+
+    private List<Color> colorList;
+    private System.Random rnd = new System.Random();
+    private string[] stringArray = new string[5];
+    private float circLeft, circRight;
+    private Transform circTransform;
+
+    private void Start()
+    {
+        circTransform = circleObjPosition.GetComponent<Transform>();
+        Transform left = circPosLeft.GetComponent<Transform>();
+        Transform right = circPosRight.GetComponent<Transform>();
+
+        circLeft = left.localPosition.x;
+        circRight = right.localPosition.x;
+
+
+        stringArray = new string[] { "one", "two", "three", "four", "five" };
+
+        colorList = new List<Color> {
+            Color.red,
+            Color.blue,
+            Color.green,
+            Color.magenta
+        };
+
+        for (int i = 0; i < colorImages.Count; i++)
+        {
+            colorImages[i].color = colorList[i];
+        }
+    }
+
+    public void UpdateRandomText()
+    {
+        randomText.text = stringArray[rnd.Next(1,5)];
+    }
+
+    public void ShuffleCircleColor()
+    {
+        SpriteRenderer circRenderer = circleColor.GetComponent<SpriteRenderer>();
+        byte r = (byte)rnd.Next(0, 255);
+        byte g = (byte)rnd.Next(0, 255);
+        byte b = (byte)rnd.Next(0, 255);
+        byte a = (byte)rnd.Next(0, 255);
+
+        circRenderer.color = new Color32(r, g, b, a);
+    }
+	
+    public void ShuffleObjectPosition()
+    {
+        float randomPosX = UnityEngine.Random.Range(circLeft, circRight);
+        circTransform.localPosition = new Vector3(randomPosX, circTransform.localPosition.y, circTransform.localPosition.z);
+    }
+
+    public void ShuffleCirclesSize()
+    {
+        int objIndex = rnd.Next(0, circleSizeObjects.Length);
+        
+        SetCircObjectActive(objIndex);
+    }
+
+    public void ShuffleImageColors()
+    {
+        colorList.Shuffle();
+
+        for (int i = 0; i < colorImages.Count; i++)
+        {
+            colorImages[i].color = colorList[i];
+        }
+    }
+
+    private void SetCircObjectActive(int index)
+    {
+        SetAllCircObjectsInactive();
+        circleSizeObjects[index].SetActive(true);
+    }
+
+    private void SetAllCircObjectsInactive()
+    {
+        for(int i=0; i < circleSizeObjects.Length; i++)
+        {
+            circleSizeObjects[i].SetActive(false);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Randomness
+# smeerws-Randomness
+
+### Project description: 
+A 2D Unity project to showcase some different examples for using Randomness (System.Random and UnityEngine.Random).
+The first example shows how to randomly change text.
+The second example randomizes the color of sprites.
+With the third and fourth example change the position and size based on a randomizer.
+The last example shows how simply shuffle colors of UI Images when using Lists. 
+
+Links: 
++ https://help.github.com/articles/fork-a-repo/
++ https://docs.unity3d.com/530/Documentation/ScriptReference/Random.html
++ https://docs.unity3d.com/530/Documentation/ScriptReference/Random-seed.html
+
+
+### Development platform: 
+Win 10, Unity Version: 2020.1.5f1, Visual Studio Version: VS Community 2019;
+
+### Target platform: 
+WebGL (reference resolution 960x600)
+
+### Visuals: 
+<img width="500" alt="ar-vr studio sc1" src="./Screenshots/randomness.JPG">
+
+### Necessary setup/execution steps: 
+Installation process, e.g. step by step instructions that I can run the project after cloning it
+None
+
+### Third party material: 
+(if used Fonts, Sounds, Music, Graphics, Materials, Code etc.)
+None
+
+### Project state: 
+100 percent
+
+### Limitations: 
+None
+
+### Lessons Learned: 
++ System.Random unequal to UnityEngine.Random
++ Using randomness to manipulate the size and the position of objects
++ Using array and lists (method shuffle)
+
+
+
+Copyright by smeerws
+
+
+
